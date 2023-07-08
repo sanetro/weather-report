c6cd4dea93fbb82601609fbb8242244eab04650b
Sanetro <50074470+sanetro@users.noreply.github.com>
2023-07-08 18:38:11 +0200

    Categorized Days: Today, Tommorow, The day after tommorow to seperated segments in panel component

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -5,14 +5,16 @@ import { HomeComponent } from '../home/home.component';
 import { WeatherService } from '../weatherStats/weather.service';
 
 export interface WeatherStatistic {
-  time: string;
+  time: Date;
   temperature: number;
   rain: number;
   windspeed: number;
   relativehumiditys: number;
 }
 
-let ELEMENT_DATA: WeatherStatistic[] = [];
+let ELEMENT_DATA_FIRST: WeatherStatistic[] = [];
+let ELEMENT_DATA_SECOND: WeatherStatistic[] = [];
+let ELEMENT_DATA_THIRD: WeatherStatistic[] = [];
 
 @Component({
   selector: 'app-panel',
@@ -22,19 +24,21 @@ let ELEMENT_DATA: WeatherStatistic[] = [];
 export class PanelComponent {
   forecastToday: Record<string, any> = {};
   forecastTomorrow: Record<string, any> = {};
-  forecastTheFollowingDay: Record<string, any> = {};
+  forecastTheDayAfterTomorrow: Record<string, any> = {};
 
   constructor(private weatherService: WeatherService) {}
 
   myData: any;
-  dataSource: WeatherStatistic[] = [];
+  dataSourceFirst: WeatherStatistic[] = [];
+  dataSourceSecond: WeatherStatistic[] = [];
+  dataSourceThird: WeatherStatistic[] = [];
 
   ngOnInit(): void {
     this.myData = this.weatherService.getData().subscribe((data) => {
       this.myData = data;
 
       // Store data
-      let times: any = this.myData['hourly']['time'];
+      let times: Date = this.myData['hourly']['time'];
       let temperatures: any = this.myData['hourly']['temperature_2m'];
       let windspeeds: any = this.myData['hourly']['windspeed_10m'];
       let rains: any = this.myData['hourly']['rain'];
@@ -54,25 +58,46 @@ export class PanelComponent {
       this.forecastTomorrow['rains'] = rains;
       this.forecastTomorrow['relativehumiditys'] = relativehumiditys;
 
-      // Forecast the following day - to panel "the following day"
-      this.forecastTheFollowingDay['times'] = times;
-      this.forecastTheFollowingDay['temperatures'] = temperatures;
-      this.forecastTheFollowingDay['windspeeds'] = windspeeds;
-      this.forecastTheFollowingDay['rains'] = rains;
-      this.forecastTheFollowingDay['relativehumiditys'] = relativehumiditys;
+      // Forecast the day after tomorrow - to panel "the day after tomorrow"
+      this.forecastTheDayAfterTomorrow['times'] = times;
+      this.forecastTheDayAfterTomorrow['temperatures'] = temperatures;
+      this.forecastTheDayAfterTomorrow['windspeeds'] = windspeeds;
+      this.forecastTheDayAfterTomorrow['rains'] = rains;
+      this.forecastTheDayAfterTomorrow['relativehumiditys'] = relativehumiditys;
 
-      for (let i = 0; i < this.forecastToday['times'].length; i++) {
-        ELEMENT_DATA.push({
-          time: this.forecastToday['times'][i],
-          temperature: this.forecastToday['temperatures'][i],
-          rain: this.forecastToday['rains'][i],
-          windspeed: this.forecastToday['windspeeds'][i],
-          relativehumiditys: this.forecastToday['relativehumiditys'][i],
-        });
-
+      for (let i = 0; i < this.forecastToday['times'].length; i++)
+      {
+        if (this.weatherService.checkTwoDates(this.weatherService.getTodayDate(), this.forecastToday['times'][i])) {
+          ELEMENT_DATA_FIRST.push({
+            time: this.forecastToday['times'][i],
+            temperature: this.forecastToday['temperatures'][i],
+            rain: this.forecastToday['rains'][i],
+            windspeed: this.forecastToday['windspeeds'][i],
+            relativehumiditys: this.forecastToday['relativehumiditys'][i],
+          });
+        }
+        if (this.weatherService.checkTwoDates(this.weatherService.getTommorowDate(), this.forecastToday['times'][i])) {
+          ELEMENT_DATA_SECOND.push({
+            time: this.forecastToday['times'][i],
+            temperature: this.forecastToday['temperatures'][i],
+            rain: this.forecastToday['rains'][i],
+            windspeed: this.forecastToday['windspeeds'][i],
+            relativehumiditys: this.forecastToday['relativehumiditys'][i],
+          });
+        }
+        if (this.weatherService.checkTwoDates(this.weatherService.getAfterTommorowDate(), this.forecastToday['times'][i])) {
+          ELEMENT_DATA_THIRD.push({
+            time: this.forecastToday['times'][i],
+            temperature: this.forecastToday['temperatures'][i],
+            rain: this.forecastToday['rains'][i],
+            windspeed: this.forecastToday['windspeeds'][i],
+            relativehumiditys: this.forecastToday['relativehumiditys'][i],
+          });
+        }
       }
-
-      this.dataSource = ELEMENT_DATA;
+      this.dataSourceFirst = ELEMENT_DATA_FIRST;
+      this.dataSourceSecond = ELEMENT_DATA_SECOND;
+      this.dataSourceThird = ELEMENT_DATA_THIRD;
     });
   }
 
